85426664c424f10231f115cf04a25759293f58c8
vheeer <vheeer@126.com>
2018-06-15 11:46:07 +0800

    init

---
+++ src/admin/controller/user.js
@@ -0,0 +1,64 @@
+const Base = require('./base.js');
+const Rest = require('./rest.js');
+//指定需要查询的字段
+const columns = ["id", "username", "gender", "birthday", "register_time", "last_login_time", "last_login_ip", "user_level_id", "nickname", "mobile", "register_ip", "avatar", "cash_paid", "is_distributor", "referee"];
+const { readAction, createAction, updateAction, deleteAction, changeImageAction } = Rest("user", columns);
+
+class top extends Base {
+  /**
+   * index action
+   * @return {Promise} []
+   */
+  async indexAction() {
+    const page = this.get('page') || 1;
+    const size = this.get('size') || 10;
+    const name = this.get('name') || '';
+
+    const model = this.model('topic');
+    const data = await model.where({title: ['like', `%${name}%`]}).order(['id DESC']).page(page, size).countSelect();
+
+    return this.success(data);
+  }
+
+  async infoAction() {
+    const id = this.get('id');
+    const model = this.model('topic');
+    const data = await model.where({id: id}).find();
+
+    return this.success(data);
+  }
+
+  async storeAction() {
+    if (!this.isPost) {
+      return false;
+    }
+
+    const values = this.post();
+    const id = this.post('id');
+
+    const model = this.model('topic');
+    values.is_show = values.is_show ? 1 : 0;
+    values.is_new = values.is_new ? 1 : 0;
+    if (id > 0) {
+      await model.where({id: id}).update(values);
+    } else {
+      delete values.id;
+      await model.add(values);
+    }
+    return this.success(values);
+  }
+
+  async destoryAction() {
+    const id = this.post('id');
+    await this.model('topic').where({id: id}).limit(1).delete();
+    // TODO 删除图片
+
+    return this.success();
+  }
+};
+top.prototype.readAction = readAction;
+top.prototype.createAction = createAction;
+top.prototype.updateAction = updateAction;
+top.prototype.deleteAction = deleteAction;
+top.prototype.changeImageAction = changeImageAction;
+module.exports = top;
---
+++ src/common/config/adapter.js
@@ -0,0 +1,151 @@
+const fileCache = require('think-cache-file');
+const {Console, File, DateFile} = require('think-logger3');
+const path = require('path');
+// const database = require('./database.js');
+const mysql = require('think-model-mysql');
+
+const isDev = think.env === 'development';
+
+/**
+ * cache adapter config
+ * @type {Object}
+ */
+exports.cache = {
+  type: 'file',
+  common: {
+    timeout: 24 * 60 * 60 * 1000 // millisecond
+  },
+  file: {
+    handle: fileCache,
+    cachePath: path.join(think.ROOT_PATH, 'runtime/cache'), // absoulte path is necessarily required
+    pathDepth: 1,
+    gcInterval: 24 * 60 * 60 * 1000 // gc interval
+  }
+};
+
+/**
+ * model adapter config
+ * @type {Object}
+ */
+exports.model = {
+  type: 'mysql',
+  common: {
+    logConnect: isDev,
+    logSql: isDev,
+    logger: msg => think.logger.info(msg)
+  }, 
+  mch: {
+    handle: mysql,
+    database: 'mch',
+    prefix: '',
+    encoding: 'utf8mb4',
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'root',
+    password: ';Classmate1133',
+    dateStrings: true
+  },
+  jiaoyang: {
+    handle: mysql,
+    database: 'nideshop_jiaoyang',
+    prefix: 'nideshop_',
+    encoding: 'utf8mb4',
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'root',
+    password: ';Classmate1133',
+    dateStrings: true
+  },
+  jisheng: {
+    handle: mysql,
+    database: 'nideshop_jisheng',
+    prefix: 'nideshop_',
+    encoding: 'utf8mb4',
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'root',
+    password: ';Classmate1133',
+    dateStrings: true
+  },
+  cat: {
+    handle: mysql,
+    database: 'nideshop_cat',
+    prefix: 'nideshop_',
+    encoding: 'utf8mb4',
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'root',
+    password: ';Classmate1133',
+    dateStrings: true
+  },
+  dapingkeji: {
+    handle: mysql,
+    database: 'nideshop_dapingkeji',
+    prefix: 'nideshop_',
+    encoding: 'utf8mb4',
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'root',
+    password: ';Classmate1133',
+    dateStrings: true
+  },
+  bishuiyuan: {
+    handle: mysql,
+    database: 'nideshop_bishuiyuan',
+    prefix: 'nideshop_',
+    encoding: 'utf8mb4',
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'root',
+    password: ';Classmate1133',
+    dateStrings: true
+  },
+  jiaju: {  
+    handle: mysql,
+    database: 'nideshop_jiaju',
+    prefix: 'nideshop_',
+    encoding: 'utf8mb4',
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'root',
+    password: ';Classmate1133',
+    dateStrings: true
+  },
+  haixin: {  
+    handle: mysql,
+    database: 'nideshop_haixin',
+    prefix: 'nideshop_',
+    encoding: 'utf8mb4',
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'root',
+    password: ';Classmate1133',
+    dateStrings: true
+  }
+};
+
+/**
+ * logger adapter config
+ * @type {Object}
+ */
+exports.logger = {
+  type: isDev ? 'console' : 'dateFile',
+  console: {
+    handle: Console
+  },
+  file: {
+    handle: File,
+    backups: 10, // max chunk number
+    absolute: true,
+    maxLogSize: 50 * 1024, // 50M
+    filename: path.join(think.ROOT_PATH, 'logs/app.log')
+  },
+  dateFile: {
+    handle: DateFile,
+    level: 'ALL',
+    absolute: true,
+    pattern: '-yyyy-MM-dd',
+    alwaysIncludePattern: true,
+    filename: path.join(think.ROOT_PATH, 'logs/app.log')
+  }
+};
